1d2c516e99e336198959f4d636f14dcec315ae0f
Kayla <kayshay005@gmail.com>
2015-08-11 11:06:55 -0400

    updates the tweet object and removed this inside array

--- team3-mapdev/js/tweetobject.js
+++ team3-mapdev/js/tweetobject.js
@@ -7,10 +7,10 @@ function tweet(lat, lng, username, message) {
 
 
 var tweetArray = [
-	{this.lat: "40.121978", this.lng: "-79.051518", this.username: "@bob_daye" this.message: "thanks for all the help"}
-	{this.lat: "4.403647", this.lng: "-62.528042", this.username: "@chris_burds" this.message: "thanks for all the help"}
-	{this.lat: "50.035639", this.lng: "14.598146", this.username: "@khristian_blue", this.message: "thanks for all the help"}
-	{this.lat: "64.415696", this.lng: "-18.624512", this.username: "@totes", this.message: "thanks for all the help"}
-	{this.lat: "52.882076", this.lng: "-105.987797", this.username: "@the_six_with_mywoes", this.message: "thanks for all the help"}
-	{this.lat: "35.781747", this.lng: "-119.347173", this.username: "@hippielife", this.message: "thanks for all the help"}
+	{"lat": "40.121978", "lng": "-79.051518", "username": "@bob_daye" "message": "thanks for all the help"}
+	{"lat": "4.403647", "lng": "-62.528042", "username": "@chris_burds" "message": "thanks for all the help"}
+	{"lat": "50.035639", "lng": "14.598146", "username": "@khristian_blue", "message": "thanks for all the help"}
+	{"lat": "64.415696", "lng": "-18.624512", "username": "@totes", "message": "thanks for all the help"}
+	{"lat": "52.882076", "lng": "-105.987797", "username": "@the_six_with_mywoes", "message": "thanks for all the help"}
+	{"lat": "35.781747", "lng": "-119.347173", "username": "@hippielife", "message": "thanks for all the help"}
 ];
