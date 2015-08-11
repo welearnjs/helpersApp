6bb4cfd5bf27a5230b769311716e0b7b3d5de149
Kayla <kayshay005@gmail.com>
2015-08-11 10:44:31 -0400

    added tweet object

---
+++ team3-mapdev/js/tweetobject.js
@@ -0,0 +1,16 @@
+function tweet(lat, lng, username, message) {
+  this.lat = lat;
+  this.lng = lng;
+  this.username = username;
+  this.message = message;
+}
+
+
+var tweetArray = [
+	{this.lat: "40.121978", this.lng: "-79.051518", this.username: "@bob_daye" this.message: "thanks for all the help"}
+	{this.lat: "4.403647", this.lng: "-62.528042", this.username: "@chris_burds" this.message: "thanks for all the help"}
+	{this.lat: "50.035639", this.lng: "14.598146", this.username: "@khristian_blue", this.message: "thanks for all the help"}
+	{this.lat: "64.415696", this.lng: "-18.624512", this.username: "@totes", this.message: "thanks for all the help"}
+	{this.lat: "52.882076", this.lng: "-105.987797", this.username: "@the_six_with_mywoes", this.message: "thanks for all the help"}
+	{this.lat: "35.781747", this.lng: "-119.347173", this.username: "@hippielife", this.message: "thanks for all the help"}
+];
