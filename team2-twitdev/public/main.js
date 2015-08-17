27db8caa288d78badd600f610bb1ba631fa31a34
James Lees <James.m.lees@gmail.com>
2015-08-17 22:52:08 +0100

    new tweets shouldn't trigger complete refresh
    When the browser first connects, to the server, it now downloads all tweets
    present on the server, then adds new tweets as they arrive. This seems far more
    efficient than fetching the entire array each time

--- team2-twitdev/public/main.js
+++ team2-twitdev/public/main.js
@@ -1,40 +1,46 @@
 $(function(){
     var arr = [];
     var socket = io( 'localhost:3000' );
-    
-    // on connection
-    socket.on( 'connect', function() { 
-        console.log( 'connected' );
+
+    var tweetListLocation = document.getElementById('tweet-list-location');
+    var tweetListNoLocation = document.getElementById('tweet-list-no-location');
+
+    // request the tweets currently held on the server from the tweets endpoint
+    $(document).ready(function() {
+
+      $.getJSON( 'tweets', function( data ){
+        var initialTweetsHtml;
+
+        if( data.tweets !== null ){
+          initialTweetsHtml= addInitialTweets( data.tweets );
+          tweetListLocation.innerHTML = initialTweetsHtml.location;
+          tweetListNoLocation.innerHTML = initialTweetsHtml.noLocation;
+        }
+
+      });
     });
-    tweets = document.getElementById('tweets');
-    socket.on('tweet', function(tweet){
-        console.log(tweet);
-//        tweets.innerHTML = tweets.innerHTML + '<br>' + tweet;
-        
+
+    // on connection
+    socket.on( 'connect', function() {
+      console.log( 'connected' );
+    });
+
+    // callback to handle arrival of new tweets
+    socket.on('tweet', function(tweetString){
+      var tweet = JSON.parse( tweetString );
+      var tweetData;
+
+      tweetData = addTweet( tweet );
+
+      if( tweetData.locationData ){
+        tweetListLocation.innerHTML += tweetData.tableRow
+      }
+      else{
+        tweetListNoLocation.innerHTML += tweetData.tableRow
+      }
     });
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // small method to iterate through an array of tweets and create a table row for
 // each one. This is just placeholder functionality to demonstrate data
