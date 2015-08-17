06c219c025add78d24a74ad6dae469908ce7b5e8
James Lees <James.m.lees@gmail.com>
2015-08-17 22:50:17 +0100

    Two stub methods to manipulate tweets
    A couple of small methods to manipulate tweet data, right now, they just return
    some simple marked-up table data, but when we have a clearer idea of that data
    required, we can adjust

--- team2-twitdev/public/main.js
+++ team2-twitdev/public/main.js
@@ -36,3 +36,54 @@ $(function(){
 
 
 
+// small method to iterate through an array of tweets and create a table row for
+// each one. This is just placeholder functionality to demonstrate data
+// definition.
+function addInitialTweets( array ){
+  var html = {
+    location: '',
+    noLocation: ''
+  };
+  var tweet;
+
+  for( var i = 0; i < array.length; i++ ){
+    tweet = addTweet( array[ i ] );
+
+    if( tweet.locationData ){
+      html.location += tweet.tableRow;
+    }
+    else{
+      html.noLocation += tweet.tableRow;
+    }
+  }
+  return html;
+}
+
+// function constructs a data object separating tweets that have data from those
+// that haven't. For the moment, by creating separate, marked up strings that
+// can easily passed to a table. This is placeholder functionality to
+// demonstrate the data definition etc.
+function addTweet( tweet ){
+  var tweetData = {
+    tableRow: '',
+    locationData: false
+  };
+
+  if( tweet.lat !== undefined && tweet.lon !== undefined ){
+    tweetData.tableRow += '<tr>' +
+      '<td>' + tweet.username + '</td>' +
+      '<td>' + tweet.lat      + '</td>' +
+      '<td>' + tweet.lon      + '</td>' +
+      '<td>' + tweet.text + '</td>' +
+    '</tr>';
+
+    tweetData.locationData = true;
+  }
+  else{
+    tweetData.tableRow += '<tr>' + 
+      '<td>' + tweet.username + '</td>' +
+      '<td>' + tweet.text + '</td>' +
+    '</tr>'
+  }
+    return tweetData;
+}
